f580f8d7751ce05869b1f86fca657598e0504609
Amar <1211amarsingh@gmail.com>
2019-06-11 11:54:03 +0530

    init

---
+++ app/src/main/java/com/example/supportin_appupdates/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.supportin_appupdates;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.appupdate.testing.FakeAppUpdateManager;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.OnSuccessListener;
+import com.google.android.play.core.tasks.Task;
+
+import static com.google.android.play.core.install.model.ActivityResult.RESULT_IN_APP_UPDATE_FAILED;
+
+public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    private static final int MY_REQUEST_CODE = 17300;
+    AppUpdateManager appUpdateManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void checkUpdate(Context context) {
+        // Creates instance of the manager.
+        appUpdateManager = AppUpdateManagerFactory.create(context);
+
+        // Returns an intent object that you use to check for an update.
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener(new OnSuccessListener<AppUpdateInfo>() {
+            @Override
+            public void onSuccess(AppUpdateInfo appUpdateInfo) {
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+
+                    Log.d("Support in-app-update", "UPDATE_AVAILABLE");
+
+                    if (appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                        requestUpdate(appUpdateInfo, AppUpdateType.IMMEDIATE);
+                    } else { //AppUpdateType.FLEXIBLE
+                        requestUpdate(appUpdateInfo, AppUpdateType.FLEXIBLE);
+                    }
+
+                } else {
+                    Log.d("Support in-app-update", "UPDATE_NOT_AVAILABLE");
+                }
+            }
+        });
+    }
+
+    private void requestUpdate(AppUpdateInfo appUpdateInfo, int flow_type) {
+        try {
+            appUpdateManager.startUpdateFlowForResult(appUpdateInfo,
+                    flow_type,
+                    this,
+                    MY_REQUEST_CODE);
+
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                Log.w("Update flow failed! ", "Result code: " + resultCode);
+                // If the update is cancelled or fails,
+                // you can request to start the update again.
+            }
+        }
+    }
+}
