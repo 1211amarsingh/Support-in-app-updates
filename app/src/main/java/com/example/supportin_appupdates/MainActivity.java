9f9beb497f07120c59da9bcb4920f4263b66300b
newhp <1211amarsingh@gmail.com>
2020-06-26 10:12:56 +0530

    minor mistake

--- app/src/main/java/com/example/supportin_appupdates/MainActivity.java
+++ app/src/main/java/com/example/supportin_appupdates/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        checkUpdate(this);
     }
 
     public void checkUpdate(Context context) {
